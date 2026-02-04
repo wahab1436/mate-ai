4cc821496424b58e3c3efe942d5c9d0ebaa6f141
Abdul Wahab <abdul00wahab1000@gmail.com>
2026-02-05 01:40:12 +0500

    Create main.py

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(
+        host=app.config.get("HOST", "0.0.0.0"),
+        port=app.config.get("PORT", 5000),
+        debug=app.config.get("DEBUG", False)
+    )
